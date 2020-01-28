e38f5abe651e40a2ebe0d4ab0ca6c43f2c702015
EJShim <soulrommel@gmail.com>
2020-01-28 17:45:05 +0900

    update

--- models/layers/mesh.py
+++ models/layers/mesh.py
@@ -1,5 +1,3 @@
-from tempfile import mkstemp
-from shutil import move
 import torch
 import numpy as np
 import os
@@ -10,18 +8,24 @@ from models.layers.mesh_prepare import fill_mesh
 class Mesh:
 
     def __init__(self, polydata):
-        self.vs = self.v_mask = self.filename = self.features = self.edge_areas = None
-        self.edges = self.gemm_edges = self.sides = None
+        self.vs = None
+        self.v_mask = None
+        self.features = None
+        self.edge_areas = []
+        self.edges = None
+        self.gemm_edges = None 
+        self.sides = None
         self.pool_count = 0
+        self.history_data = None
+
+        #Initialize
         fill_mesh(self, polydata)        
-        self.history_data = None        
+                
         self.init_history()        
 
     def extract_features(self):
         return self.features
-    def extract_gemm_edges(self):
-        return self.gemm_edges
-
+        
     def merge_vertices(self, edge_id):
         self.remove_edge(edge_id)
         edge = self.edges[edge_id]
@@ -68,33 +72,6 @@ class Mesh:
         self.__clean_history(groups, torch_mask)
         self.pool_count += 1    
 
-
-    def __get_cycle(self, gemm, edge_id):
-        cycles = []
-        for j in range(2):
-            next_side = start_point = j * 2
-            next_key = edge_id
-            if gemm[edge_id, start_point] == -1:
-                continue
-            cycles.append([])
-            for i in range(3):
-                tmp_next_key = gemm[next_key, next_side]
-                tmp_next_side = self.sides[next_key, next_side]
-                tmp_next_side = tmp_next_side + 1 - 2 * (tmp_next_side % 2)
-                gemm[next_key, next_side] = -1
-                gemm[next_key, next_side + 1 - 2 * (next_side % 2)] = -1
-                next_key = tmp_next_key
-                next_side = tmp_next_side
-                cycles[-1].append(next_key)
-        return cycles
-
-    def __cycle_to_face(self, cycle, v_indices):
-        face = []
-        for i in range(3):
-            v = list(set(self.edges[cycle[i]]) & set(self.edges[cycle[(i + 1) % 3]]))[0]
-            face.append(v_indices[v])
-        return face
-
     def init_history(self):
         self.history_data = {
                                'groups': [],
@@ -120,7 +97,7 @@ class Mesh:
     def get_groups(self):
         return self.history_data['groups'].pop()
 
-    def get_occurrences(self):
+    def get_occurrences(self): 
         return self.history_data['occurrences'].pop()
     
     def __clean_history(self, groups, pool_mask):
